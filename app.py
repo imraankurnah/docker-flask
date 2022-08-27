9b5a408fa28ddf62c14f979dcceb10afe8d369a9
imraankurnah <112204092+imraankurnah@users.noreply.github.com>
2022-08-27 19:32:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
-from flask import Flask, render_template
-import os
+import json
+from flask import Flask, request, jsonify
+import requests
 
 app = Flask(__name__)
 
 
 @app.route('/test', methods=['GET'])
-def home():
-    return 'ttt'
-
-
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+def test():
+    r = requests.get('https://google.com')
+    print(r.text)
+    return str(r.status_code)
