16aaed39bb906a40791cbdd35e27430805c30ae0
imraankurnah <112204092+imraankurnah@users.noreply.github.com>
2022-08-27 19:47:08 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,6 @@ app = Flask(__name__)
 
 @app.route('/test', methods=['GET'])
 def test():
-    r = requests.get('https://google.com')
-    print(r.text)
-    return str(r.status_code)
+    # r = requests.get('https://google.com')
+    # print(r.text)
+    return 'str(r.status_code)'
