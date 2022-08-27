c8f0d9fce2b3c51c824a4851d325f88a1a3031e7
imraankurnah <112204092+imraankurnah@users.noreply.github.com>
2022-08-27 19:56:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,11 @@ app = Flask(__name__)
 
 @app.route('/test', methods=['GET'])
 def test():
-    # r = requests.get('https://google.com')
-    # print(r.text)
-    return 'str(r.status_code)'
+    r = requests.get('https://google.com')
+    print(r.text)
+    return str(r.status_code)
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
